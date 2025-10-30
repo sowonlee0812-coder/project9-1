c5d1a94a674df6f6f28bd9b19cfcbfa9e1e9329d
sowonlee0812-coder <sowon.lee0812@gmail.com>
2025-10-30 15:41:13 +0900

    project9-2(1)

--- project9-1/project9-1/main.c
+++ project9-1/project9-1/main.c
@@ -1,21 +1,24 @@
 
 #include <stdlib.h>
 #include <stdio.h>
+#define SIZE 5
 
 int main(int argc, char * argv[]) {
     
+    //SIZE명의 학생에 대한 시험성적
     int i;
-    int grade[5];
+    int testScore[SIZE];
+    printf("%i명 점수를 입력하세요.\n", SIZE);
     
-    grade[0] =10;
-    grade[1] =20;
-    grade[2] =30;
-    grade[3] =40;
-    grade[4] =50;
-    
-    for(i=0;i<5;i++)
-        printf("grade[%d] = %d\n", i, grade[i]);
-    
+    //점수 입력
+    for(i=0;i<SIZE;i++)
+        scanf("%d", &testScore[i]);
+
+    //점수 화면 출력 (반복문 활용)
+    for(i=0;i<SIZE;i++)
+        printf("grade[%i] = %i\n", i, testScore[i]);
+  
+            
     system("PAUSE");
     return 0;
     
